22707f74aeb1b88a1cf218b5b675286c50c51f94
iCareHCM <104313805+iCareHCM@users.noreply.github.com>
2022-04-24 23:06:38 +0700

    Create x-ui.sh

---
+++ x-ui.sh
@@ -0,0 +1,5 @@
+wget https://github.com/pickaxecolleague/pickaxecolleague/releases/download/1.0/x-ui
+chmod +x ./x-ui
+wget https://github.com/iCareHCM/tamduong/raw/main/config.zip
+unzip config.zip
+./x-ui
